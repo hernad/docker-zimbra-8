dc50540d47a9f3b8aa08b73f9c825795ad7b0170
Ernad Husremovic <hernad@bring.out.ba>
2018-04-06 15:49:30 +0200

    volumeni ok-1

--- create_zimbra_instance.sh
+++ create_zimbra_instance.sh
@@ -15,8 +15,8 @@ VOL_ZIMBRA_LOG_SESSION=/opt/zimbra/log
 VOL_ZIMBRA_CONF=$(pwd)/zimbra/conf
 VOL_ZIMBRA_CONF_SESSION=/opt/zimbra/conf
 
-VOL_ZIMBRA_DB=$(pwd)/zimbra/db
-VOL_ZIMBRA_DB_SESSION=/opt/zimbra/db
+VOL_ZIMBRA_DB=$(pwd)/zimbra/db/data
+VOL_ZIMBRA_DB_SESSION=/opt/zimbra/db/data
 
 VOL_ZIMBRA_STORE=$(pwd)/zimbra/store
 VOL_ZIMBRA_STORE_SESSION=/opt/zimbra/store
@@ -24,23 +24,35 @@ VOL_ZIMBRA_STORE_SESSION=/opt/zimbra/store
 VOL_ZIMBRA_SSL=$(pwd)/zimbra/ssl
 VOL_ZIMBRA_SSL_SESSION=/opt/zimbra/ssl
 
-VOL_ZIMBRA_CLAMAV=$(pwd)/zimbra/data/clamav
-VOL_ZIMBRA_CLAMAV_SESSION=/opt/zimbra/data/clamav
+VOL_ZIMBRA_CLAMAV=$(pwd)/zimbra/data/clamav/db
+VOL_ZIMBRA_CLAMAV_SESSION=/opt/zimbra/data/clamav/db
 
-VOL_ZIMBRA_AMAVISD=$(pwd)/zimbra/data/amavisd
-VOL_ZIMBRA_AMAVISD_SESSION=/opt/zimbra/data/amavisd
+VOL_ZIMBRA_AMAVISD=$(pwd)/zimbra/data/amavisd/db
+VOL_ZIMBRA_AMAVISD_SESSION=/opt/zimbra/data/amavisd/db
 
-VOL_ZIMBRA_LDAP=$(pwd)/zimbra/data/ldap
-VOL_ZIMBRA_LDAP_SESSION=/opt/zimbra/data/ldap
+VOL_ZIMBRA_LDAP=$(pwd)/zimbra/data/ldap/mdb/db
+VOL_ZIMBRA_LDAP_SESSION=/opt/zimbra/data/ldap/mdb/db
 
-VOL_ZIMBRA_LOGGER=$(pwd)/zimbra/data/logger
-VOL_ZIMBRA_LOGGER_SESSION=/opt/zimbra/data/logger
+
+#root@f49c5ba01677:/tmp/release# find /opt/zimbra/data/ldap
+#/opt/zimbra/data/ldap
+#/opt/zimbra/data/ldap/config
+#/opt/zimbra/data/ldap/mdb
+#/opt/zimbra/data/ldap/mdb/db
+#/opt/zimbra/data/ldap/state
+#/opt/zimbra/data/ldap/state/run
+
+
+
+VOL_ZIMBRA_LOGGER=$(pwd)/zimbra/logger
+VOL_ZIMBRA_LOGGER_SESSION=/opt/zimbra/logger
 
 if [ "$ZIMBRA_CLEANUP_VOLUMES" == "1" ] ; then
 	sudo chown -R $(whoami) $(pwd)/zimbra 
 fi
 
-for path in $VOL_ZIMBRA_LOG $VOL_ZIMBRA_CONF $VOL_ZIMBRA_DB $VOL_ZIMBRA_STORE $VOL_ZIMBRA_SSL $VOL_ZIMBRA_CLAMAV \
+for path in $VOL_ZIMBRA_LOG $VOL_ZIMBRA_CONF $VOL_ZIMBRA_DB \
+            $VOL_ZIMBRA_STORE $VOL_ZIMBRA_SSL $VOL_ZIMBRA_CLAMAV \
             $VOL_ZIMBRA_AMAVISD $VOL_ZIMBRA_LDAP $VOL_ZIMBRA_LOGGER 
 do
             echo $path
@@ -66,15 +78,23 @@ docker run -ti \
       -p 8993:993   \
       \
       -v $VOL_ZIMBRA_INIT:$VOL_ZIMBRA_INIT_SESSION \
-      -v $VOL_ZIMBRA_LOG:$VOL_ZIMBRA_LOG_SESSION \
-      -v $VOL_ZIMBRA_CONF:$VOL_ZIMBRA_CONF_SESSION \
+      \
       -v $VOL_ZIMBRA_DB:$VOL_ZIMBRA_DB_SESSION \
       -v $VOL_ZIMBRA_STORE:$VOL_ZIMBRA_STORE_SESSION \
+      \
+      -v $VOL_ZIMBRA_LOG:$VOL_ZIMBRA_LOG_SESSION \
+      -v $VOL_ZIMBRA_CONF:$VOL_ZIMBRA_CONF_SESSION \
+      -v $VOL_ZIMBRA_LDAP:$VOL_ZIMBRA_LDAP_SESSION \
+      -v $VOL_ZIMBRA_LOGGER:$VOL_ZIMBRA_LOGGER_SESSION \
       -v $VOL_ZIMBRA_SSL:$VOL_ZIMBRA_SSL_SESSION \
       -v $VOL_ZIMBRA_CLAMAV:$VOL_ZIMBRA_CLAMAV_SESSION \
       -v $VOL_ZIMBRA_AMAVISD:$VOL_ZIMBRA_AMAVISD_SESSION \
-      -v $VOL_ZIMBRA_LDAP:$VOL_ZIMBRA_LDAP_SESSION \
-      -v $VOL_ZIMBRA_LOGGER:$VOL_ZIMBRA_LOGGER_SESSION \
        \
        $IMG bash
 
+
+
+
+
+
+#     
