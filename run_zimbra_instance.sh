f350e5bf1568a559d5811499243a32f57a088a3d
Ernad Husremovic <hernad@bring.out.ba>
2018-04-06 17:00:27 +0200

    /init/run_instance

--- run_zimbra_instance.sh
+++ run_zimbra_instance.sh
@@ -2,41 +2,10 @@
 
 source zimbra_common.sh
 
-if [ "$ZIMBRA_CLEANUP_VOLUMES" == "1" ] ; then
-	sudo chown -R $(whoami) $(pwd)/zimbra 
-fi
-
-for path in $VOL_ZIMBRA_LOG $VOL_ZIMBRA_CONF $VOL_ZIMBRA_DB \
-            $VOL_ZIMBRA_STORE $VOL_ZIMBRA_SSL \
-            $VOL_ZIMBRA_CLAMAV $VOL_ZIMBRA_AMAVISD \
-            $VOL_ZIMBRA_POSTFIX $VOL_ZIMBRA_POSTFIX_2 \
-            $VOL_ZIMBRA_LDAP $VOL_ZIMBRA_LDAP_2 $VOL_ZIMBRA_LDAP_3 \
-            $VOL_ZIMBRA_LOGGER \
-            $VOL_ZIMBRA_ZIMLETS
-do
-            echo $path
-            [ "$ZIMBRA_CLEANUP_VOLUMES" == "1" ] && echo cleanup $path && rm -rf $path 
-done
-
-
-
-#-e MAILBOX_HOST_NAME=mailbox \
-
 docker rm -f $NAME
 
 docker run -ti \
       --name $NAME \
-      -e ZIMBRA_DEFAULT_DOMAIN=zimbra.test \
-      -e ZIMBRA_HOST_NAME=zimbra-1.zimbra.test \
-      -e TIME_ZONE_ID=Europe\\\/Sarajevo \
-      -e ADMIN_PW=test123 \
-      -e LDAP_ADMIN_PW=zimbra \
-      -e LDAP_AMAVIS_PW=zimbra \
-      -e LDAP_POSTFIX_PW=zimbra \
-      -e LDAP_REPLICATION_PW=zimbra \
-      -e LDAP_ROOT_PW=zimbra \
-      -e LDAP_BES_PW=zimbra \
-      -e LDAP_NGINX_PW=zimbra \
       -p 7071:7071  \
       -p 8143:143   \
       -p 9443:443   \
@@ -48,7 +17,6 @@ docker run -ti \
       -v $VOL_ZIMBRA_DB:$VOL_ZIMBRA_DB_SESSION \
       -v $VOL_ZIMBRA_POSTFIX:$VOL_ZIMBRA_POSTFIX_SESSION \
       -v $VOL_ZIMBRA_POSTFIX_2:$VOL_ZIMBRA_POSTFIX_2_SESSION \
-      \
       -v $VOL_ZIMBRA_STORE:$VOL_ZIMBRA_STORE_SESSION \
       \
       -v $VOL_ZIMBRA_LOG:$VOL_ZIMBRA_LOG_SESSION \
@@ -67,8 +35,3 @@ docker run -ti \
        $IMG bash
 
 
-
-
-
-
-#     
