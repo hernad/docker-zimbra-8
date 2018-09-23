13f15d8911708f90b36ad309df488ccd9c8047f9
Ernad Husremovic <hernad@bring.out.ba>
2018-09-23 11:59:54 +0200

    zimbra_88_89

--- init/upgrade/zimbra_88_89.sh
+++ init/upgrade/zimbra_88_89.sh
@@ -1,6 +1,6 @@
 apt-get update -y
 apt-get upgrade -y
-FILE_BASE=zcs-8.8.9_GA_3019.UBUNTU16_64.20180809160254
+FILE_BASE="zcs-8.8.9_GA_3019.UBUNTU16_64.20180809160254"
 FILE_TGZ=$FILE_BASE.tgz
 
 if [ ! -f $FILE_TGZ ] ; then
@@ -16,4 +16,7 @@ cd /
 tar cvf /docker_etc/zimbra_saveconfig.tar.gz
 
 
-/opt/zimbra/libexec/zmsetup.pl -c /init/config
+if  /opt/zimbra/libexec/zmsetup.pl -c /init/config ; then
+   # saveconfig after upgrade
+   tar cvf /docker_etc/zimbra_saveconfig.tar.gz
+fi
