26fa07b0bafd992b672e42f251bbf4cafcdefca6
Ernad Husremovic <hernad@bring.out.ba>
2018-09-23 12:59:50 +0200

    show zimbra version

--- enter_into_zimbra_instance.sh
+++ enter_into_zimbra_instance.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+
+docker exec  $ZIMBRA_INSTANCE_NAME \
+   /bin/bash -c 'su zimbra -c "/opt/zimbra/bin/zmcontrol -v"'
+
+
 docker exec  -ti \
        $ZIMBRA_INSTANCE_NAME \
        /bin/bash
