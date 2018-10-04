2193ae07cddb0bed59510ebb2b052da71e4f56ff
Ernad Husremovic <hernad@bring.out.ba>
2018-10-04 11:31:30 +0200

    cleanup

--- enter_into_zimbra_instance.sh
+++ enter_into_zimbra_instance.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ -z "$ZIMBRA_INSTANCE_NAME" ] ; then
+   echo "definisati ZIMBRA_INSTANCE_NAME"
+   exit 1
+fi
 
 docker exec  $ZIMBRA_INSTANCE_NAME \
    /bin/bash -c 'su zimbra -c "/opt/zimbra/bin/zmcontrol -v"'
